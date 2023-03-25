abd9920f98aee40f71acc929420b64233f9ef57b
cimbaIG <mihael.cindori@gmail.com>
2023-03-25 16:53:06 +0100

    Strings and string methods

--- Part1/ConsoleApp1/Program.cs
+++ Part1/ConsoleApp1/Program.cs
@@ -126,6 +126,72 @@ namespace ConsoleApp1
 
             // Add commas and decimals
             Console.WriteLine("Commas: {0:n4}", 2300);
+
+            // ----------------- Strings -----------------
+
+            // Strings store a series of characters
+            string randString = "This is a string";
+
+            // Get number of characters in string
+            Console.WriteLine("String length: {0}", randString.Length);
+
+            // Check if string contains another string
+            Console.WriteLine("String contains 'is': ",
+                randString.Contains("is"));
+
+            // Get index of substring inside the string
+            Console.WriteLine("Index of 'is': {0}", randString.IndexOf("is"));
+
+            // Remove 10 characters from the string starting at an index 6
+            Console.WriteLine("Remove string : {0}",
+                randString.Remove(10, 6));
+
+            // Add a string starting at index 10
+            Console.WriteLine("Insert string: {0}",
+                randString.Insert(10, "short "));
+
+            // Replace string with another string
+            Console.WriteLine("Replace string: {0}",
+                randString.Replace("string", "sentence"));
+
+            // Compare strings and ignore case
+            // < 0 : str1 preceeds str2
+            // = : Zero
+            // > 0 : str2 preceeds str1
+            Console.WriteLine("Compare A to B: {0}",
+                String.Compare("A", "B", StringComparison.OrdinalIgnoreCase));
+
+            // Check if strings are equal
+            Console.WriteLine("A = a: {0}",
+                String.Compare("A", "a", StringComparison.OrdinalIgnoreCase));
+
+            // Add padding left
+            Console.WriteLine("Pad Left: {0}", randString.PadLeft(20, '.'));
+
+            // Add padding right
+            Console.WriteLine("Pad Right: {0}", randString.PadRight(20, '.'));
+
+            // Trim whitespace
+            Console.WriteLine("Trim: {0}", randString.Trim());
+
+            // Make uppercase
+            Console.WriteLine("Uppercase: {0}", randString.ToUpper());
+
+            // Make lowecase
+            Console.WriteLine("Lowercase: {0}", randString.ToLower());
+
+            // Use Format to create strings
+            string newString = String.Format("{0} saw a {1} {2} in the {3}.",
+                "Paul", "rabbit", "eating", "field");
+
+            // You can add newlines with \n and join strings with +
+            Console.Write(newString + "\n");
+
+            // Other escape characters
+            // \' \" \\ \t \a
+
+            // Verbatim strings ignore escape characters
+            Console.WriteLine(@"Exactly What I Typed\n");
         }
     }
 }
