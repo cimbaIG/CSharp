4f8eb63f44c2b34efb575caef2f5b7f7a8cdba06
cimbaIG <mihael.cindori@gmail.com>
2023-03-26 00:05:40 +0100

    If-else statements

--- Part1/ConsoleApp1/Program.cs
+++ Part1/ConsoleApp1/Program.cs
@@ -315,6 +315,73 @@ namespace ConsoleApp1
             {
                 Console.WriteLine("CopyTo : {0} ", m);
             }
+
+            // ----------------- If-Else statements -----------------
+            // Relational Operators : > < >= <= == !=
+            // Logical Operators : && || !
+
+            int age = 17;
+
+            if (age >= 5 && age <= 7)
+            {
+                Console.WriteLine("Go to elementary school.");
+            }
+            else if (age > 7 && age < 13)
+            {
+                Console.WriteLine("Go to middle school.");
+            }
+            else if (age > 13 && age < 19)
+            {
+                Console.WriteLine("Go to high school.");
+            }
+            else
+            {
+                Console.WriteLine("Go to college.");
+            }
+
+            if (age < 14 || age > 67)
+            {
+                Console.WriteLine("You sholdn't work.");
+            }
+
+            Console.WriteLine("! true = " + (!true));
+
+            // Ternary operator - it assigns 1st value if true, and 2nd value
+            // otherwise.
+            bool canDrive = age >= 16 ? true : false;
+
+            // Switch is used when you have limited options. The only way to use
+            // ranges is to stack the possible values.
+            switch (age)
+            {
+                case 1:
+                case 2:
+                    Console.WriteLine("Go to Day Care.");
+                    break;
+                case 3:
+                case 4:
+                    Console.WriteLine("Go to Preschool.");
+                    break;
+                case 5:
+                    Console.WriteLine("Go to Kindergarten.");
+                    break;
+                default:
+                    Console.WriteLine("Go to another school.");
+                    // You can also jump out of a switch with goto
+                    goto OtherSchool;
+            }
+
+        OtherSchool:
+            Console.WriteLine("Elementary, Middle, High School.");
+
+            // How to compare strings using Equals?
+            string name2 = "Derek";
+            string name3 = "Derek";
+
+            if (name2.Equals(name3,StringComparison.Ordinal))
+            {
+                Console.WriteLine("Names are equal.");
+            }
         }
     }
 }
