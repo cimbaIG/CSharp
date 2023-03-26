a5b8d6207e4468243bd0b97019c7f283a881a838
cimbaIG <mihael.cindori@gmail.com>
2023-03-27 00:24:09 +0200

    Datetime and Timespan

--- Part1/ConsoleApp1/Program.cs
+++ Part1/ConsoleApp1/Program.cs
@@ -682,6 +682,27 @@ namespace ConsoleApp1
             Console.WriteLine($"5.0 + 4.5 = {GetSum2(5.0, 4.5)}");
             Console.WriteLine($"5 + 4 = {GetSum2(5, 4)}");
             Console.WriteLine($"5 + 4 = {GetSum2("5", "4")}");
+
+            // ----------------- Datetime and timespan -----------------
+
+            // Datetime is used to define dates
+            DateTime awesomeDate = new DateTime(1974, 12, 21);
+            Console.WriteLine($"Day of Week: {awesomeDate.DayOfWeek}");
+
+            // We can change date values
+            awesomeDate = awesomeDate.AddDays(4);
+            awesomeDate = awesomeDate.AddMonths(1);
+            awesomeDate = awesomeDate.AddYears(1);
+            Console.WriteLine($"New Date: {awesomeDate.Date}");
+
+            // Timespan is used to define a time.
+            TimeSpan lunchTime = new TimeSpan(12, 30, 0);
+            Console.WriteLine($"Lunchtime: {lunchTime.ToString()}");
+
+            // We can change time values
+            lunchTime = lunchTime.Subtract(new TimeSpan(0, 15, 0));
+            lunchTime = lunchTime.Add(new TimeSpan(1, 0, 0));
+            Console.WriteLine($"New Lunchtime: {lunchTime.ToString()}");
         }
     }
 }
