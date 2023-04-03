aed8f4c61fcc514edeabc73c2c9cd12351755f6c
Mihael Cindori <mihael.cindori@gmail.com>
2023-04-03 13:59:12 +0200

    Lambda function

---
+++ Part10/Lambda/Lambda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        delegate double doubleIt(double val);
+
+        static void Main(string[] args)
+        {
+            doubleIt dblIt = x => x * 2;
+            Console.WriteLine($"5 * 2 = {dblIt(5)}");
+
+            Console.ReadLine();
+        }
+    }
+}
