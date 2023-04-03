039b5643cf3031070dac3b953df75ef7c1f72a2a
Mihael Cindori <mihael.cindori@gmail.com>
2023-04-03 17:10:17 +0200

    IEnumerable explained

---
+++ Part11/Enumerables/Enumerables/AnimalFarm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerables
+{
+    // IEnumerable provides us iteration over a collection
+
+    class AnimalFarm : IEnumerable
+    {
+        // List that holds objects of class Animal
+        private List<Animal> animalList = new List<Animal>();
+
+        public AnimalFarm(List<Animal> animalList)
+        {
+            this.animalList = animalList;
+        }
+
+        public AnimalFarm() { }
+
+        // Indexer for AnimalFarm created using this[]
+        public Animal this[int index]
+        {
+            get { return (Animal)animalList[index]; }
+            set { animalList.Insert(index, value); }
+        }
+
+        // Returns the number of values in the collection
+        public int Count
+        {
+            get
+            {
+                return animalList.Count;
+            }
+        }
+
+        // Returns an enumerator that is used to iterate through
+        // the collection
+        public IEnumerator GetEnumerator()
+        {
+            return animalList.GetEnumerator();
+        }
+    }
+}
---
+++ Part11/Enumerables/Enumerables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Learn more about IEnumerable, Indexer, Enumerator, Operator Overloading,
+// Custom Casting, Anonymous Types and more...
+
+namespace Enumerables
+{
+    // Indexers allows us to access items like arrays
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an AnimalFarm object
+            AnimalFarm myAnimals = new AnimalFarm();
+
+            // Add animals to myAnimals
+            myAnimals[0] = new Animal("Wilbur");
+            myAnimals[1] = new Animal("Templeton");
+            myAnimals[2] = new Animal("Gander");
+            myAnimals[3] = new Animal("Charlotte");
+
+            // Print out animal names
+            foreach (Animal animal in myAnimals) 
+            { 
+                Console.WriteLine(animal.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
