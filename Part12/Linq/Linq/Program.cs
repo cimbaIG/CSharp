ecba9b48a4f3b17d75dc83047a5f7be2e5d620aa
Mihael Cindori <mihael.cindori@gmail.com>
2023-04-05 18:30:44 +0200

    LINQ - inner join and group join

--- Part12/Linq/Linq/Program.cs
+++ Part12/Linq/Linq/Program.cs
@@ -31,6 +31,8 @@ namespace Linq
 
             QueryCollection();
 
+            QueryAnimalData();
+
             Console.ReadKey();
         }
 
@@ -104,7 +106,7 @@ namespace Linq
 
             return arrayGT20;
         }
-
+        
         static void QueryArrayList()
         {
             ArrayList famAnimals = new ArrayList()
@@ -170,6 +172,133 @@ namespace Linq
             }
 
             Console.WriteLine();
+        }
+
+        static void QueryAnimalData()
+        {
+            Animal[] animals = new[]
+            {
+                new Animal
+                {
+                    Name = "German Shepherd",
+                    Height = 25,
+                    Weight = 77,
+                    AnimalID = 1
+                },
+                new Animal
+                {
+                    Name = "Chihuahua",
+                    Height = 7,
+                    Weight = 4.4,
+                    AnimalID = 2
+                },
+                new Animal
+                {
+                    Name = "Saint Bernard",
+                    Height = 30,
+                    Weight = 200,
+                    AnimalID = 3
+                },
+                new Animal
+                {
+                    Name = "Pug",
+                    Height = 12,
+                    Weight = 16,
+                    AnimalID = 4
+                },
+                new Animal
+                {
+                    Name = "Beagle",
+                    Height = 15,
+                    Weight = 23,
+                    AnimalID = 5
+                }
+            };
+
+            Owner[] owners = new[]
+            {
+                new Owner
+                {
+                    Name = "Doug Parks",
+                    OwnerID = 1
+                },
+                new Owner
+                {
+                    Name = "Sally Smith",
+                    OwnerID = 2
+                },
+                new Owner
+                {
+                    Name = "Paul Brooks",
+                    OwnerID = 3
+                }
+            };
+
+            // Remove name and height
+            var nameHeight = from a in animals
+                             select new
+                             { 
+                                 a.Name, 
+                                 a.Height 
+                             };
+
+            // Convert nameHeight to an object array
+            Array arrNameHeight = nameHeight.ToArray();
+
+            foreach (var i in arrNameHeight)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            Console.WriteLine();
+
+            // Create an INNER JOIN
+            // Join info in owners and animals using equal values for IDs.
+            // Store values for both animal and owner.
+            var innerJoin = from animal in animals
+                            join owner in owners on animal.AnimalID
+                            equals owner.OwnerID
+                            select new
+                            {
+                                OwnerName = owner.Name,
+                                AnimalName = animal.Name
+                            };
+
+            foreach (var i in innerJoin)
+            {
+                Console.WriteLine("{0} owns {1}.",
+                    i.OwnerName, i.AnimalName);
+            }
+
+            Console.WriteLine();
+
+            // Create a GROUP INNER JOIN
+            // Get all animals and put them in a newly created owner group if
+            // their IDs match the owners ID.
+            var groupJoin = from owner in owners
+                            orderby owner.OwnerID
+                            join animal in animals on owner.OwnerID
+                            equals animal.AnimalID into ownerGroup
+                            select new
+                            {
+                                Owner = owner.Name,
+                                Animals = from owner2 in ownerGroup
+                                          orderby owner2.Name
+                                          select owner2
+                            };
+
+            int totalAnimals = 0;
+
+            foreach (var ownerGroup in groupJoin)
+            {
+                Console.WriteLine(ownerGroup.Owner);
+
+                foreach (var animal in ownerGroup.Animals)
+                {
+                    totalAnimals++;
+                    Console.WriteLine("* {0}", animal.Name);
+                }
+            }
         }
     }
 }
